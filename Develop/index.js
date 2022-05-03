c36f8447a80ab9883856e8186f26b290d165d45c
Jason Cardenas <Jason.Cardenas@ftcsc.org>
2022-05-03 07:52:10 -0400

    add prompt and packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,56 @@
-const inquirer = require("inquirer");
-
 // TODO: Include packages needed for this application
-const inquire = require(inquirer)
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: 'input',
+        name: 'github',
+        message: "Please enter your Github username."
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address'
+    },
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: "Please enter the title of your project."
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please enter the description of your project."
+    },
+    {
+        type: 'list',
+        name: 'liscense',
+        message: 'Select the license for your project:',
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2", "BSD3", "None"],
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please provide any instructions for installation or commands to install any dependences'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide the input to run any tests'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe any information a user might need to know to use this repo'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please list all contributors'
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
