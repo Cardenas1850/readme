647ed5bfb5ca89fd1834e1578d0844b5808758d8
Jason Cardenas <Jason.Cardenas@ftcsc.org>
2022-05-03 08:08:14 -0400

    add and fixed license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,7 +18,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license !== "None") {
+        return (
+            `<h2 id='license'>License</h2>
+      Copyright © ${license}. All rights reserved.
+      
+      Lincensed under the ${license} license.`
+        )
+    }
+    return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
